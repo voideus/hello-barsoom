09643d0a36bed8fc35f72741a7ea907c97ab0a2f
bikash tamang <tbikash227@gmail.com>
2024-08-20 12:21:06 +0000

    set output

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import * as github from '@actions/github';
 try {
     const name = core.getInput('name');
     const output_value = `Hello ${name}!`;
-    core.setOutput('greeting ', output_value)
+    core.setOutput('greeting', output_value)
 } catch (error) {
     core.setFailed(error.message)
 }
